73560e4136f2af24c960c6e418b828933a71b25a
Juan Martín <juanmega24@gmail.com>
2023-11-20 17:17:06 +0100

    interactions with depth work

--- Assignment2/helper.rb
+++ Assignment2/helper.rb
@@ -1,4 +1,5 @@
 require 'rest-client'
+require 'json'
 
 def get_interactions_FromGene_IntoHash(geneid, hash)
 
@@ -23,10 +24,21 @@ def get_interactions_FromGene_IntoHash(geneid, hash)
             b = line.split("\t")[5][/A[Tt]\d[Gg]\d\d\d\d\d/, 0]
             intType = line.split("\t")[11][/(M[^"]*)/, 0]
 
-            # Hacer caso de que a y b son iguales. Eso y pasar a minúsculas geneid
+        # NOT ALL PROTEINS HAVE LOCUS NAMES, IF ABOVE CODE DOESNT CAPTURE THE LOCUS, IT GETS DISCARDED
+            # if a == nil || b == nil
+            #     a = line.split("\t")[0][/([A-Z][^\t]*)/, 0]
+            #     b = line.split("\t")[1][/([A-Z][^\t]*)/, 0]
+            #     response = RestClient::Request.execute(  #  or you can use the 'fetch' function we created last class
+            #         method: :get,
+            #         url: "http://togows.org/entry/ebi-uniprot/#{a}/dr.json")
+            #     data = JSON.parse(response.body)
+            #     puts data[0]["TAIR"] #[/A[Tt]\d[Gg]\d\d\d\d\d/, 0]
+            # end
+
+            # Hacer caso de que no esté el locus id en los alias
             x = 0
             [a, b].each do |gen|
-                if x == 0 && (gen != geneid || a == b) && (intType == "MI:0407" || intType == "MI:0915")
+                if x == 0 && (gen != geneid || a == b) && (intType == "MI:0407" || intType == "MI:0915") && gen != nil
                     interactions << gen
                     x = 1
                 end
@@ -66,17 +78,19 @@ end
 # end
 
 def get_interactions_FromFile_IntoHash_WithDepth(file, hash, depth)
-
+    n = 0
+    lines = File.foreach(file).count
     IO.readlines(file)[2..3].each do |geneid|
-
+        n += 1
+        puts "#{n}/#{lines}"
         geneid.strip!.downcase!.capitalize!
     
         hash = get_interactions_FromGene_IntoHash(geneid, hash)
 
         names = []
-        (0..depth-1).each do |i|
-            hash.keys do |gen|
+        (0..depth-1).each do
 
+            hash.keys.each do |gen|
                 if !names.include?(gen) && hash[gen].kind_of?(Array)
                     hash[gen].each do |name|
                         unless hash.key?(name)
@@ -87,7 +101,6 @@ def get_interactions_FromFile_IntoHash_WithDepth(file, hash, depth)
             end
             names << hash.keys
         end
-        
     end
     return hash
 end

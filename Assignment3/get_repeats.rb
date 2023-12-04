342d2a66be890765cfa2d7ad70e253ce196808e4
Juan Martín <juanmega24@gmail.com>
2023-12-04 12:51:51 +0100

    new features appended correctly

--- Assignment3/get_repeats.rb
+++ Assignment3/get_repeats.rb
@@ -1,21 +1,45 @@
 require 'bio'
 
-embl = Bio::EMBL.new(Bio::Fetch::EBI.query("ensemblgenomesgene",'AT5g19120','raw', 'embl'))
-
+embl = Bio::EMBL.new(Bio::Fetch::EBI.query("ensemblgenomesgene",'AT5g54270','raw', 'embl'))
+# explicar que no nos guardamos el objeto embl porque para hacer un gff no nos hace falta
 bioseq = embl.to_biosequence
 
 sequence =  bioseq.seq
-puts sequence
 
 bioseq.features.each do |feature|
 
-    featuretype = feature.feature
-    next unless featuretype == "exon"
+    next unless feature.feature == "exon" && /^[0-9]+..[0-9]+/.match?(feature.position)
 
-    range = Range.new(eval(feature.position).begin() -1, eval(feature.position).end) 
-    positions_exon = sequence[range].enum_for(:scan, /cttctt/).map { Regexp.last_match.begin(0) }
+    range = Range.new(eval(feature.position).begin() -1, eval(feature.position).end() -1) 
+    positions_plus_exon = sequence[range].enum_for(:scan, /cttctt/).map { Regexp.last_match.begin(0) }
     # source: https://stackoverflow.com/questions/5241653/ruby-regex-match-and-get-positions-of#5241843
-    
-    puts positions_exon
 
+    positions_plus_exon.each do |pos|
+        feat = Bio::Feature.new('myrepeat', Range.new(range.begin()+pos+1, range.begin()+pos+6).to_s)
+        feat.append(Bio::Feature::Qualifier.new('repeat_motif', 'CTTCTT'))
+        feat.append(Bio::Feature::Qualifier.new('strand', '+'))
+        bioseq.features << feat
+    end
+
+    positions_neg_exon = sequence[range].enum_for(:scan, /aagaag/).map { Regexp.last_match.begin(0) }
+
+    positions_neg_exon.each do |pos|
+        feat = Bio::Feature.new('myrepeat', Range.new(range.begin()+pos+1, range.begin()+pos+6).to_s)
+        feat.append(Bio::Feature::Qualifier.new('repeat_motif', 'CTTCTT'))
+        feat.append(Bio::Feature::Qualifier.new('strand', '-'))
+        bioseq.features << feat
+    end
+
+end
+
+bioseq.features.each do |feature|
+    featuretype = feature.feature
+    next unless featuretype == "myrepeat"
+    position = feature.position
+    puts "FEATURE #{featuretype} @ POSITION = #{position}"
+
+    qual = feature.assoc            # feature.assoc gives you a hash of Bio::Feature::Qualifier objects 
+                                    # i.e. qualifier['key'] = value  for example qualifier['gene'] = "CYP450")
+    puts "Associations = #{qual}"
+    # skips the entry if "/translation=" is not found
 end

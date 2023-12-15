da7b5893ff1376db9824e0f420a7ad181ef11c51
Juan <juanmega24@gmail.com>
2023-12-15 12:53:13 +0100

    bug fixed; this is a backup; currently waiting to get the final output

--- Assignment4/README.md
+++ Assignment4/README.md
@@ -1,2 +1,19 @@
-# Retos
-Work on Bioinformatics Programming Challenges course
+# How to run
+ruby reciprocal_best_BLAST.rb 
+
+Script runs for: minutes
+
+If you are going to run it: Delete results.table
+
+# Results
+Reciprocal BLAST best hits are stored in results.table
+
+These are derived from blastx and tblastn with parameters that use soft filtering and a Smith–Waterman alignment. This was used following the indications of the following paper:
+
+Gabriel Moreno-Hagelsieb, Kristen Latimer, Choosing BLAST options for better detection of orthologs as reciprocal best hits, Bioinformatics, Volume 24, Issue 3, February 2008, Pages 319–324, https://doi.org/10.1093/bioinformatics/btm585
+
+# Documentation
+Exercises are carried out with a simple script that uses some BioRuby tools learned in class.
+
+Documentation using YARD was deem unnecesary, as no functions were defined. So script is documented using 
+traditional comments.
--- Assignment4/reciprocal_best_BLAST.rb
+++ Assignment4/reciprocal_best_BLAST.rb
@@ -1,47 +1,70 @@
 require 'bio'
 
+start = Time.now
 # Creating BLAST factories:
+puts "Creating BLAST factories..."
 factory_pombe_aa = Bio::Blast.local('blastx', 'Sequence_files/pombe_aa', '-F "m S"' '-s T') 
 factory_thali_na = Bio::Blast.local('tblastn', 'Sequence_files/thali_na', '-F "m S"' '-s T') 
 # https://doi.org/10.1093/bioinformatics/btm585
+factories = Time.now
+puts "Time elapsed: #{factories - start} seconds"
 
 # Iterating over each S.pombe protein from local proteome:
+puts "Iterating over each S.pombe protein from local proteome..."
 pombe_fasta = Bio::FlatFile.open(Bio::FastaFormat, 'Sequence_files/pombe_aa.fa')
-x = 0# XDDDDDDDDDDDDDDddd
+n = 0
 pombe_fasta.each_entry do |pombe_fasta_entry|
 
     # Quering A.thaliana local proteome for BLAST best hit:
     hits_thali = factory_thali_na.query(pombe_fasta_entry.seq.to_s).hits
     next if hits_thali.empty? || hits_thali.first.evalue > 0.05
 
-    x += 1
     # Appending best hit to results file if it's e-value < 0.5
     File.open("./results.table", 'a') { |file| file.puts("#{hits_thali.first.definition.split("|")[0].strip}\t#{pombe_fasta_entry.entry_id}") }
+    n += 1
+    puts "Protein: #{n}/total"
 end
-puts x
+puts n
+pombe_proteins = Time.now
+puts "Time elapsed: #{(pombe_proteins - start)/60} minutes"
 
-# Reading all logged hits in order to check if they are reciprocal:
+# For some reason ".each_entry" method of flat file doesn't reset to the first entry 
+# each time the loop is runned. So I'm replacing it with an array:
 thali_fasta = Bio::FlatFile.open(Bio::FastaFormat, 'Sequence_files/thali_na.fa')
-results = IO.readlines("./results.table")
-new_file_lines = "" # storing good hits to write later 
+thali_entries_array = []
+thali_fasta.each_entry do |thali_fasta_entry|
+    thali_entries_array << thali_fasta_entry
+end
+
+puts "Checking if logged hits are reciprocal.."
+rec_start = Time.now
+# Reading all logged hits in order to check if they are reciprocal:
+new_file_lines = "" # storing reciprocal hits to write later 
 #source: https://stackoverflow.com/questions/17638621/deleting-a-specific-line-in-a-text-file)
-results.each do |line|
+n = 0
+IO.readlines("./results.table").each do |line|
 
-    thali_fasta.each_entry do |thali_fasta_entry|
-        puts thali_fasta_entry.entry_id, line.split("\t")[0]
-        if thali_fasta_entry.entry_id == line.split("\t")[0]
+    thali_entries_array.each do |thali_entry|
+        if thali_entry.entry_id == line.split("\t")[0]
+            
+            # Once the fasta entry is found, it's queried in the S.pombe database:
+            first_hit_pombe = factory_pombe_aa.query(thali_entry.seq.to_s).hits.first
 
-            first_hit_pombe = factory_pombe_aa.query(thali_fasta_entry.seq.to_s).hits.first
+            break if first_hit_pombe == nil
 
-            next if first_hit_pombe == nil
-            puts first_hit_pombe.definition.split("|")[0]
-            puts line.split("\t")[1].strip
+            # If the result is reciprocal, the hit is kept:
             if first_hit_pombe.definition.split("|")[0] == line.split("\t")[1].strip
                 new_file_lines += line
+            else break
             end
         end
     end
+    n += 1
+    puts "Protein: #{n}/total"
 end
+check_rec = Time.now
+puts "Time elapsed: #{(check_rec - rec_start)/60} minutes"
+puts "Total time elapsed: #{(check_rec - start)/60} minutes"
 
-# Overwriting results table with headers:
+# Overwriting results table with reciprocal hits and adding headers:
 File.open("./results.table", 'w') { |file| file.puts("A.thaliana\tS.pombe\n#{new_file_lines}") }
